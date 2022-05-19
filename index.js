00074cbc79e13445e1b2f0b5e2ca167303b10b8d
Phil Prater <pprater92@gmail.com>
2022-05-19 11:02:08 -0700

    made some dummy book data. first version of constructor and a read-toggle for Book objs.

--- index.js
+++ index.js
@@ -3,13 +3,31 @@
 // books are going to be displayed to user and user will be able to add new books.
 // user will also be able to edit books and toggle read/unread status
 
-let myLibary = [];
+let myLibary = [
+  {title:'Dune', author:'Frank Herbert', read:true}, 
+  {title:'It', author:'Stephen King', read:false}
+];
 
 // Book constructor
-function Book() {
+function Book(title, author) {
+  this.title = title 
+  this.author = author
+  this.read = false
+}
 
+// Toggle a Book's read status
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
 }
 
 function addBookToLibrary() {
+
+}
+
+function removeBookFromLibrary() {
+
+}
+
+function displayBooks() {
 
 }
