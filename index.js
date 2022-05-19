af2af16579f22b71e42069a2e6988fd7ce0ee4fc
Phil Prater <pprater92@gmail.com>
2022-05-19 10:15:35 -0700

    first commit. basic boilerplates.

---
+++ index.js
@@ -0,0 +1,15 @@
+// == js-bookshelf ==
+// book objs are going to be stored in a simple array.
+// books are going to be displayed to user and user will be able to add new books.
+// user will also be able to edit books and toggle read/unread status
+
+let myLibary = [];
+
+// Book constructor
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
